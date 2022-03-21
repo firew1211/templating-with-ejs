f27d23ff01d44ae5f1c8df2983c297ca48caf112
Gracia Biselele <91652558+Gbiselele24@users.noreply.github.com>
2022-03-21 04:56:57 +0000

    add

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = 'Our Home Page';
+  res.render('pages/index',{'title':title});
+});
+
+
+///about-us
+app.get('/about-us', (req, res) => {
+  var title = 'Our about Us Page';
+  res.render('pages/about-us',{'title':title});
+});
+
+///chessecake
+app.get('/cheesecake', (req, res) => {
+  var title = 'Our cheesecake Page';
+  res.render('pages/cheesecake',{'title':title});
+});
+
+
+app.get('/apple pie', (req, res) => {
+  var title = 'Our apple pie Page';
+  res.render('pages/apple-pie',{'title':title});
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

00570ae02cfab0ee5423cc882236242e432c7436
Firew Kahsay <91629098+firew1211@users.noreply.github.com>
2022-03-25 09:01:12 +0000

    Add

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var data = require('./data/test.json');
+
 app.set('view engine','ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
 app.use(express.static(__dirname));
-
 
 app.get('/', (req, res) => {
   var title = 'Our Home Page';
   res.render('pages/index',{'title':title});
 });
 
+//users index is our list page
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
 
 ///about-us
 app.get('/about-tigray', (req, res) => {
@@ -43,6 +62,11 @@ app.get('/tigray-leader', (req, res) => {
   res.render('pages/tigray-leader',{'title':title});
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+
+app.listen(port, () =>{
+  console.log(`Example app listening on port${port}`);
+  console.log(data);
 });
+
+
